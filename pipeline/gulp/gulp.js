3464c6f22a80d1028613c4c579fa2f21156f972d
Emerson Hinterholz <emersonhinterholz@gmail.com>
2020-05-02 15:17:46 -0300

    gulp

--- pipeline/gulp/gulp.js
+++ pipeline/gulp/gulp.js
@@ -5,24 +5,22 @@ var del = require('del');
 var gulp = require('gulp');
 var htmlmin = require('gulp-htmlmin');
 var runSequence = require('run-sequence');
-var sass = require('gulp-sass');
-var uglify = require('gulp-uglify');
 
-gulp.task('clean', () => del(['dist']));
+gulp.task('clean', () => del(['../../dist']));
 
 gulp.task('styles', function () {
-    return gulp.src('./css/main.css')
+    return gulp.src('../../html/css/main.css')
         .pipe(csso())
-        .pipe(gulp.dest('./dist/css'))
+        .pipe(gulp.dest('../../dist/css'))
 });
 
 gulp.task('pages', function() {
-return gulp.src(['./index.html'])
+return gulp.src(['../../html/index.html'])
     .pipe(htmlmin({
         collapseWhitespace: true,
         removeComments: true
     }))
-    .pipe(gulp.dest('./dist'));
+    .pipe(gulp.dest('../../dist'));
 });
 
 gulp.task('default', ['clean'], function () {
