f8bab3615fabdc7e8e6a1b6fd3b983f89710fb70
Emerson Hinterholz <emersonhinterholz@gmail.com>
2025-07-19 18:53:15 -0300

    build: modernizing and organizing build

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 
 ./stop.sh
 
-docker run -d -p 80:80 -v $(pwd)/html:/usr/share/nginx/html:ro -v /$(pwd)/conf:/etc/nginx/conf.d:ro --name ehint-runtime nginx
+docker run -d -p 80:80 -v $(pwd)/src:/usr/share/nginx/html:ro -v /$(pwd)/conf:/etc/nginx/conf.d:ro --name ehint-runtime nginx
