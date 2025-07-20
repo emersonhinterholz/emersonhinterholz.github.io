5112238c85cc4a09c43e257da0693c0f1ed9beb9
Emerson Hinterholz <emersonhinterholz@gmail.com>
2025-07-20 16:01:56 -0300

    build: removing instrumentation

--- build-scripts/replace-instrumentation-key.js
+++
@@ -1,8 +0,0 @@
-const replace = require('replace-in-file');
-const options = {
-  files: './dist/**/*.*',
-  from: /%%instrumentationKey%%/g,
-  to: '4c64e005-e066-424d-b05b-2ed4869bb2b7'
-};
-
-replace.replaceInFileSync(options);
